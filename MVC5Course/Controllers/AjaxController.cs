9d73aacf1f9764c804edc3ace40539f83a337018
myericho <myericho.ho@gmail.com>
2015-11-28 09:52:07 +0800

    加入aja變化

---
+++ MVC5Course/Controllers/AjaxController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class AjaxController : Controller
+    {
+        // GET: Ajax
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [OutputCache(NoStore = false, Duration = 0)]
+        public ActionResult gettime()
+        {
+            return Content(DateTime.Now.ToString());
+        }
+    }
+}
--- MVC5Course/Models/Product.Partial.cs
+++ MVC5Course/Models/Product.Partial.cs
@@ -2,36 +2,40 @@
 namespace MVC5Course.Models
 {
 
-using System;
+    using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
 
-[MetadataType(typeof(ProductMetaData))]
-public partial class Product
-{
-}
 
-public partial class ProductMetaData
-{
+    [MetadataType(typeof(ProductMetaData))]
+    public partial class Product
+    {
+    }
 
-    [Required]
+    public partial class ProductMetaData
+    {
 
-    public int ProductId { get; set; }
-    
-    [StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
+        [Required]
 
-    public string ProductName { get; set; }
+        public int ProductId { get; set; }
 
-    public Nullable<decimal> Price { get; set; }
+        [StringLength(80, ErrorMessage = "欄位長度不得大於 80 個字元")]
 
-    public Nullable<bool> Active { get; set; }
+        public string ProductName { get; set; }
 
-    public Nullable<decimal> Stock { get; set; }
+        public Nullable<decimal> Price { get; set; }
+
+        public Nullable<bool> Active { get; set; }
+
+        [DisplayName("庫存量")]
+        [UIHint("Decimal.庫存")]
+        public Nullable<decimal> Stock { get; set; }
 
 
 
-    public virtual ICollection<OrderLine> OrderLine { get; set; }
+        public virtual ICollection<OrderLine> OrderLine { get; set; }
 
-}
+    }
 
 }
